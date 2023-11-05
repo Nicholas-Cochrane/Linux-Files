451a96837a2293cb2786f4a6207bdf227d4106ef
Nicholas-Cochrane <nicholascochrane25@gmail.com>
2023-11-04 20:02:11 -0700

    added docker connection checking bash script

---
+++ dockerping.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+idvar=$(docker ps | grep '   markusmcnugen/qbittorrentvpn' | awk '{print $1}')
+echo $idvar
+returnvar=$(docker exec $idvar bash -c "curl -Is  --connect-timeout 15 https://linuxhint.com/" | wc -c)
+if (( $returnvar > 0 )); then
+	echo "container can see internet"
+else
+	echo "container can not see internet"
+	echo "Attempting Restart"
+	docker restart $idvar
+fi
