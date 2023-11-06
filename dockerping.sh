6e4cedbdd6baa123e66278c57cbfc3a8e685a3a0
Nicholas-Cochrane <nicholascochrane25@gmail.com>
2023-11-05 17:06:21 -0800

    Added case for if container is not running

--- dockerping.sh
+++ dockerping.sh
@@ -2,6 +2,11 @@
 idvar=$(docker ps | grep '   markusmcnugen/qbittorrentvpn' | awk '{print $1}')
 echo "[" $(date) "]"
 echo $idvar
+idsize=$(echo $idvar | wc -c)
+if (( $idsize <= 1 )); then
+	echo "ERROR: No Such Container 'markusmcnugen/qbittorrentvpn'"
+	exit 1
+fi
 returnvar=$(docker exec $idvar bash -c "curl -Is  --connect-timeout 15 https://linuxhint.com/" | wc -c)
 if (( $returnvar > 0 )); then
 	echo "container can see internet"
