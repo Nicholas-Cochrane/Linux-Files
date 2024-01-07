e7c8e1342ab1c9ede0e2c7cfaa33974a0411f94d
Nicholas-Cochrane <nicholascochrane25@gmail.com>
2024-01-06 22:54:00 -0800

    improved dockerping.sh

--- dockerping.sh
+++ dockerping.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 idvar=$(docker ps | grep '   markusmcnugen/qbittorrentvpn' | awk '{print $1}')
 echo "[" $(date) "]"
-echo $idvar
 idsize=$(echo $idvar | wc -c)
 if (( $idsize <= 1 )); then
+	echo $idvar
 	echo "ERROR: No Such Container 'markusmcnugen/qbittorrentvpn'"
 	exit 1
 fi
 returnvar=$(docker exec $idvar bash -c "curl -Is  --connect-timeout 15 https://linuxhint.com/" | wc -c)
-if (( $returnvar > 0 )); then
-	echo "container can see internet"
-else
+if (( $returnvar <= 0 )); then
+	echo $idvar
 	echo "container can not see internet"
 	echo "Attempting Restart"
 	docker restart $idvar
