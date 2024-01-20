58f61e5ac50c05fd2d24ae092a82d072290faf47
Nicholas-Cochrane <nicholascochrane25@gmail.com>
2024-01-19 16:46:35 -0800

    added user for sql server

--- docker_groups.sh
+++ docker_groups.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo useradd --system --uid 998 --gid 100 qbittorrent -c "qBittorrentvpn docker container system user"
 sudo useradd --system --uid 997 --gid 100 jellyfin -c "jellyfin docker container system user"
+sudo useradd --system --uid 996 --gid 100 sqlcontainer -c "sql docker container system user"
 echo "May require system restart to avoid issues"
