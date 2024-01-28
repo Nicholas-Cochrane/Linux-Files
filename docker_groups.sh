8f080197fe4ceda48d9d2ccb5fbb95b79b8c20d9
Nicholas-Cochrane <nicholascochrane25@gmail.com>
2024-01-27 18:00:33 -0800

    added user

--- docker_groups.sh
+++ docker_groups.sh
@@ -2,4 +2,5 @@
 sudo useradd --system --uid 998 --gid 100 qbittorrent -c "qBittorrentvpn docker container system user"
 sudo useradd --system --uid 997 --gid 100 jellyfin -c "jellyfin docker container system user"
 sudo useradd --system --uid 996 --gid 100 sqlcontainer -c "sql docker container system user"
+sudo useradd --system --uid 995 --gid 100 grafanacontainer -c "grafana docker container system user"
 echo "May require system restart to avoid issues"
