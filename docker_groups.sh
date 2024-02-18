eee17b6d154320438aefd6d36fbafbc25fcabc9e
Nicholas-Cochrane <nicholascochrane25@gmail.com>
2024-02-17 20:39:12 -0800

    added firefly iii

--- docker_groups.sh
+++ docker_groups.sh
@@ -3,4 +3,5 @@ sudo useradd --system --uid 998 --gid 100 qbittorrent -c "qBittorrentvpn docker 
 sudo useradd --system --uid 997 --gid 100 jellyfin -c "jellyfin docker container system user"
 sudo useradd --system --uid 996 --gid 100 sqlcontainer -c "sql docker container system user"
 sudo useradd --system --uid 995 --gid 100 grafanacontainer -c "grafana docker container system user"
+sudo useradd --system --uid 994 --gid 100 fireflycontainer -c "firefly iii docker container system user"
 echo "May require system restart to avoid issues"
